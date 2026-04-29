9d003fefd9a3b36889eff35a4338f279719058e8
Paul-isol <isolpaul@gmail.com>
2026-04-29 21:39:00 +0530

    basic express backend created

---
+++ server/server.js
@@ -0,0 +1,23 @@
+import express from "express"
+import cors from "cors"
+import multer from "multer"
+import "dotenv/config"
+import connectDB from "./config/db.js"
+const app = express()
+const PORT = process.env.PORT || 8000
+
+// Middleware
+app.use(cors())
+app.use(express.json())
+app.use(multer().none())
+
+// Routes
+app.get("/", (req, res) => {
+    res.send("Backend is running...")
+})
+
+await connectDB()
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`)
+})
